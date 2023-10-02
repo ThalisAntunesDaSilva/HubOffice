306154132ce4df33b7321854b8f65b87848d64a3
MatheusMayor <matheus.mayor@hotmail.com>
2023-10-02 20:00:26 -0300

    atualizacoes tela de cadastro e login

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -3,22 +3,33 @@ import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 import styles from './styles';
 
 const Register = () => {
+    const [cadastroTitulo, setCadastroTitulo] = useState('');
     const [nome, setNome] = useState('');
+    const [username, setUserName] = useState('');
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+    const [tipoUsuario, setTipoUsuario] = useState('');
 
     const handleCadastro = () => {
         // Lógica para lidar com o cadastro aqui
-        console.log('Realizar o cadastro com os seguintes dados:', nome, email, senha);
+        console.log('Realizar o cadastro com os seguintes dados:', cadastroTitulo, nome, username, email, senha);
     };
 
     return (
         <View style={styles.container}>
+            <Text style={styles.titulo}>Cadastro</Text>
+
             <TextInput
                 style={styles.input}
                 placeholder="Nome"
                 value={nome}
                 onChangeText={(text) => setNome(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholder="UserName"
+                value={username}
+                onChangeText={(text) => setUserName(text)}
             />
             <TextInput
                 style={styles.input}
@@ -33,11 +44,28 @@ const Register = () => {
                 value={senha}
                 onChangeText={(text) => setSenha(text)}
             />
+            <View style={styles.tipoUsuarioContainer}>
+                <TouchableOpacity
+                    style={[styles.tipoUsuarioButton, tipoUsuario === 'proprietario' ? styles.selectedButton : null]}
+                    onPress={() => setTipoUsuario('proprietario')}
+                >
+                    <Text style={styles.tipoUsuarioButtonText}>Proprietário</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[styles.tipoUsuarioButton, tipoUsuario === 'locatario' ? styles.selectedButton : null]}
+                    onPress={() => setTipoUsuario('locatario')}
+                >
+                    <Text style={styles.tipoUsuarioButtonText}>Locatário</Text>
+                </TouchableOpacity>
+            </View>
+
             <TouchableOpacity style={styles.button} onPress={handleCadastro}>
-                <Text style={styles.buttonText}>Cadastrar</Text>
+                <Text style={styles.buttonText}>Cadastre-se</Text>
+                <Text>Já tem conta?Faça seu login</Text>
             </TouchableOpacity>
         </View>
     );
 };
+
 
 export default Register;
---
+++ src/pages/Register/styles.js
@@ -0,0 +1,115 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        paddingTop: 90,
+        paddingHorizontal: 10,
+    },
+    titulo: {
+        fontFamily: 'Alata-Regular',
+        fontSize: 30,
+        color: "black",
+        marginBottom: 5,
+        marginRight: '50px'
+
+    },
+
+    titleRegisterLabel: {
+        fontFamily: 'Alata-Regular',
+        fontSize: 30,
+        color: "#A9ADB7",
+        marginBottom: 5,
+        marginRight: '50px',
+    },
+
+    label: {
+        fontFamily: 'Alata-Regular',
+        fontSize: 16,
+        marginBottom: 5,
+        alignSelf: "flex-start",
+        color: "black",
+    },
+
+    input: {
+        width: "100%",
+        height: 45,
+        borderColor: "gray",
+        borderWidth: 2,
+        marginBottom: 5,
+        paddingHorizontal: 10,
+        borderRadius: 8,
+    },
+
+    inputRegister: {
+        width: "100%",
+        fontSize: 16,
+        fontFamily: 'Alata-Regular',
+        marginBottom: 5,
+        color: "#A7A6A5",
+        width: "100%",
+    },
+    buttonRegister: {
+        width: "90%",
+        backgroundColor: "#EC4760",
+        borderRadius: 20,
+        fontFamily: "Alata-Regular",
+        height: 70,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 20,
+    },
+    buttonRegisterText: {
+        color: "#F4F6F5",
+        fontFamily: "Alata-Regular",
+        fontSize: 16,
+        borderRadius: 16,
+        marginBottom: 0,
+    },
+    listFormRegister: {
+        width: "100%",
+    },
+    signupContainer: {
+        marginTop: 0,
+        flexDirection: "row",
+        alignItems: "center",
+        paddingBottom: 3,
+        marginVertical: 20,
+    },
+    signupRegisterText: {
+        fontSize: 16,
+        color: "#A7A6A5",
+        fontFamily: "Alata-Regular",
+    },
+    signupLinkRegisterText: {
+        color: "#EC4760",
+        textDecorationLine: "underline",
+        marginLeft: 5,
+        fontFamily: "Alata-Regular",
+    },
+    tipoUsuarioContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 10,
+    },
+    tipoUsuarioButton: {
+        flex: 1,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 5,
+        marginRight: 5,
+    },
+    selectedButton: {
+        backgroundColor: 'blue',
+        borderColor: 'blue',
+    },
+    tipoUsuarioButtonText: {
+        textAlign: 'center',
+        color: 'black',
+    },
+});
